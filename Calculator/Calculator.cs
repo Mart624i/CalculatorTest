1df4bdaa62bfaf19ff72c7b24cbd7cf5421bec96
Thea <thea2805@gmail.com>
2022-02-03 13:39:33 +0100

    Committing Accumulator + Clear() method

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -4,24 +4,40 @@ namespace Calculator
 {
     public class Calculator
     {
+        public double Accumulator { get; private set; }
+
+        public void Clear()
+        {
+            Accumulator = 0;
+        }
         public double Add(double a, double b)
         {
+            Clear();
+            Accumulator = a + b;
             return a + b;
         }
 
         public double Subtract(double a, double b)
         {
+            Clear();
+            Accumulator = a - b;
             return a - b;
         }
 
         public double Multiply(double a, double b)
         {
+            Clear();
+            Accumulator = a * b;
             return a * b;
         }
 
         public double Power(double a, double b)
         {
+            Clear();
+            Accumulator = Math.Pow(a, b);
             return Math.Pow(a, b);
         }
+
+
     }
 }
